e6d4672f30dabebfb441e19062ef61c83ce74283
Robert Kowalski <rok@kowalski.gd>
2013-06-28 21:42:22 +0200

    add tests for folder & bugfix
    addition of tests for the folder
    fix callback naming in async discern
    in case of an error, the callback now returns the error object

---
+++ test/folder.js
@@ -0,0 +1,32 @@
+var assert = require('assert')
+var discern = require('../')
+
+var folder = discern.folder
+describe('folder(path, cb)', function () {
+  it('should call a callback with an git-url', function (done) {
+    folder(__dirname + '/../', function (er, u) {
+      assert.ok(er === null)
+      assert.ok(/discern.git$/.test(u))
+      done()
+    })
+  })
+  it('should call a callback with an error', function (done) {
+    folder(__dirname + '/enteEnteEnte', function (er, u) {
+      assert.ok(er)
+      assert.ok(er.code === 'ENOENT')
+      done()
+    })
+  })
+})
+
+var folderSync = discern.folder.sync
+describe('discern.sync(path)', function () {
+  it('returns a git-url', function () {
+    var res = folderSync(__dirname + '/../')
+    assert.ok(/discern.git$/.test(res))
+  })
+  it('should return null in case of an error', function () {
+    var res = folderSync(__dirname + './enteEnteEnte')
+    assert.ok(res === null)
+  })
+})
